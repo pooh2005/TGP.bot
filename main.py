bbe1147563188e58b6ecd12aae51e15be8f2068e
pooh2005 <131867904+pooh2005@users.noreply.github.com>
2024-10-10 23:42:03 +0700

    main.py

--- main.py
+++ main.py
@@ -5,28 +5,47 @@ from discord.ext import commands
 
 from myserver import server_on
 
-# กำหนดการตั้งค่าบอท
 intents = discord.Intents.default()
-bot = commands.Bot(command_prefix="!", intents=intents)
+bot = commands.Bot(command_prefix=".tgp_", intents=intents)
 
 # กำหนดเวลา 2 ชั่วโมงครึ่ง (9000 วินาที)
 TIME_INTERVAL = 9000  # 2 ชั่วโมงครึ่ง
+notification_active = False
+notification_task = None  # ตัวแปรสำหรับจัดเก็บ task ของ asyncio
 
 @bot.event
 async def on_ready():
     print(f'Logged in as {bot.user.name}')
-    bot.loop.create_task(notification_task())
 
-# ฟังก์ชันสำหรับแจ้งเตือนทุก ๆ 2 ชั่วโมงครึ่ง
-async def notification_task():
-    await bot.wait_until_ready()
-    channel = bot.get_channel("1272892370513559654")  # ใส่ ID ของแชนแนลที่จะส่งแจ้งเตือน
-    while not bot.is_closed():
-        await channel.send("ถึงเวลาเข้าเวรแล้ว!")
-        await asyncio.sleep(TIME_INTERVAL)  # รอ 2 ชั่วโมงครึ่งก่อนแจ้งเตือนครั้งต่อไป
+# ฟังก์ชันสำหรับแจ้งเตือน
+async def notify(channel):
+    while notification_active:
+        await channel.send("ถึงเวลาเข้าเวรแล้ว!..กรุณาเข้าเวรด้วย")
+        await asyncio.sleep(TIME_INTERVAL)
+
+# คำสั่งเริ่มการแจ้งเตือน
+@bot.command()
+async def start(ctx):
+    global notification_active, notification_task
+    if not notification_active:
+        notification_active = True
+        channel = ctx.channel
+        notification_task = bot.loop.create_task(notify(channel))
+        await ctx.send("ระบบแจ้งเตือนเริ่มต้นแล้ว!")
+    else:
+        await ctx.send("ระบบแจ้งเตือนทำงานอยู่แล้ว!")
+
+# คำสั่งหยุดการแจ้งเตือน
+@bot.command()
+async def stop(ctx):
+    global notification_active, notification_task
+    if notification_active:
+        notification_active = False
+        if notification_task is not None:
+            notification_task.cancel()  # หยุด Task ที่กำลังทำงานอยู่
+        await ctx.send("ระบบแจ้งเตือนถูกหยุดแล้ว!")
+    else:
+        await ctx.send("ไม่มีการแจ้งเตือนที่ทำงานอยู่ในขณะนี้!")
 
 # รันบอท
-
-server_on()
-
 bot.run(os.getenv('TOKEN'))
